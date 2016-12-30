0d9826cb22a7c9217797a7e7e03ea96c509d9d12
thanhnt <tatthanh.dev@gmail.com>
2016-12-30 17:22:44 +0700

    #5: update fontawesome 4.7

--- resources/views/layouts/admin/styles.blade.php
+++ resources/views/layouts/admin/styles.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="{!! \URLHelper::asset('libs/bootstrap/css/bootstrap.min.css', 'admin') !!}">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 <link rel="stylesheet" href="{!! \URLHelper::asset('libs/adminlte/css/AdminLTE.min.css', 'admin') !!}">
 <link rel="stylesheet" href="{!! \URLHelper::asset('libs/adminlte/css/skins/_all-skins.min.css', 'admin') !!}">
