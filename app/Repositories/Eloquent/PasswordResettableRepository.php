42bf2c2817bcd7748fa3eeb3fd04a747b43ab686
thanhnt <tatthanh.dev@gmail.com>
2017-01-04 10:41:52 +0700

    fix PasswordReset

--- app/Repositories/Eloquent/PasswordResettableRepository.php
+++ app/Repositories/Eloquent/PasswordResettableRepository.php
@@ -1,35 +1,40 @@
 <?php
-
 namespace App\Repositories\Eloquent;
 
 use App\Repositories\PasswordResettableRepositoryInterface;
 use Illuminate\Auth\Passwords\DatabaseTokenRepository;
 
-class PasswordResettableRepository extends DatabaseTokenRepository implements PasswordResettableRepositoryInterface
-{
+class PasswordResettableRepository extends DatabaseTokenRepository implements PasswordResettableRepositoryInterface {
     protected $tableName = 'password_resets';
-
     protected $hashKey = 'random';
-
     protected $expires = 60;
 
-    protected function getDatabaseConnection()
-    {
-        return $connection = app()['db']->connection();
+    public function __construct() {
+        parent::__construct( $this->getDatabaseConnection(), $this->tableName, $this->hashKey, $this->expires );
     }
 
-    public function __construct()
-    {
-        parent::__construct($this->getDatabaseConnection(), $this->tableName, $this->hashKey, $this->expires);
-    }
-
-    public function findEmailByToken($token)
-    {
-        $token = (array) $this->getTable()->where('token', $token)->first();
-        if ($this->tokenExpired($token)) {
+    public function findEmailByToken( $token ) {
+        $token = $this->getTable()
+                      ->where( 'token', $token )
+                      ->first();
+        if( empty( $token ) ) {
+            return null;
+        }
+        if( $this->tokenExpired(
+            [
+                'email'      => $token->email,
+                'token'      => $token->token,
+                'created_at' => $token->created_at,
+            ]
+        )
+        ) {
             return null;
         }
 
-        return $token['email'];
+        return $token[ 'email' ];
+    }
+
+    protected function getDatabaseConnection() {
+        return $connection = app()[ 'db' ]->connection();
     }
 }
