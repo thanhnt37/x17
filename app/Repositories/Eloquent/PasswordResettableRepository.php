e4066f188d476dc4c0240a8a099c2037a847648b
thanhnt <tatthanh.dev@gmail.com>
2017-09-10 14:19:23 +0700

    reset password as laravel 5.4

--- app/Repositories/Eloquent/PasswordResettableRepository.php
+++ app/Repositories/Eloquent/PasswordResettableRepository.php
@@ -4,37 +4,21 @@ namespace App\Repositories\Eloquent;
 use App\Repositories\PasswordResettableRepositoryInterface;
 use Illuminate\Auth\Passwords\DatabaseTokenRepository;
 
-class PasswordResettableRepository extends DatabaseTokenRepository implements PasswordResettableRepositoryInterface {
+class PasswordResettableRepository extends DatabaseTokenRepository implements PasswordResettableRepositoryInterface
+{
     protected $tableName = 'password_resets';
-    protected $hashKey = 'random';
-    protected $expires = 60;
 
-    public function __construct() {
-        parent::__construct( $this->getDatabaseConnection(), $this->tableName, $this->hashKey, $this->expires );
+    protected $hashKey   = 'random';
+
+    protected $expires   = 1440;
+
+    public function __construct()
+    {
+        parent::__construct($this->getDatabaseConnection(), app()['hash'], $this->tableName, $this->hashKey, $this->expires);
     }
 
-    public function findEmailByToken( $token ) {
-        $token = $this->getTable()
-                      ->where( 'token', $token )
-                      ->first();
-        if( empty( $token ) ) {
-            return null;
-        }
-        if( $this->tokenExpired(
-            [
-                'email'      => $token->email,
-                'token'      => $token->token,
-                'created_at' => $token->created_at,
-            ]
-        )
-        ) {
-            return null;
-        }
-
-        return $token[ 'email' ];
-    }
-
-    protected function getDatabaseConnection() {
-        return $connection = app()[ 'db' ]->connection();
+    protected function getDatabaseConnection()
+    {
+        return $connection = app()['db']->connection();
     }
 }
