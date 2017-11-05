7ea17a4d1d1294efd1f4b94b9dddae18f0edc666
thanhnt <tatthanh.dev@gmail.com>
2017-11-05 18:36:27 +0700

    fix issue popper.js in laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,36 +12,31 @@ var mix = require('laravel-mix');
  */
 
 mix
-    .options({
-        minimize:true
-    })
     .autoload({
         jquery: ['$', 'jQuery', 'jquery', 'window.jQuery'],
         tether: ['Tether', 'window.Tether'],
-        'popper.js': ['Popper']
+        'popper.js/dist/umd/popper.js': ['Popper', 'window.Popper']
     })
 
     .styles(
         [
-            'bower_components/bootstrap/dist/css/bootstrap.min.css',
-            'bower_components/tether/dist/css/tether.min.css',
-            'bower_components/roboto-fontface/css/roboto/roboto-fontface.css',
-            'bower_components/components-font-awesome/css/font-awesome.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/roboto-fontface/css/roboto/roboto-fontface.css',
+            'node_modules/components-font-awesome/css/font-awesome.min.css',
             'resources/assets/web/2017/css/layout.css',
             'resources/assets/web/2017/css/home.css'
         ],
         'public/static/web/2017/css/styles.css'
     )
 
-    .copyDirectory('bower_components/components-font-awesome/fonts', 'public/static/web/2017/fonts')
-    .copyDirectory('bower_components/roboto-fontface/fonts', 'public/static/web/fonts')
+    .copyDirectory('node_modules/components-font-awesome/fonts', 'public/static/web/2017/fonts')
+    .copyDirectory('node_modules/roboto-fontface/fonts', 'public/static/web/fonts')
 
     .js(
         [
-            'bower_components/jquery/dist/jquery.slim.min.js',
-            'bower_components/tether/dist/js/tether.min.js',
-            'bower_components/popper.js/dist/popper.min.js',
-            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.slim.min.js',
+            'node_modules/popper.js/dist/umd/popper.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'resources/assets/web/2017/js/layout.js'
         ],
         'public/static/web/2017/js/script.js'
