a3286ef19bfb005810cf6ba78f01592c4d9420d7
thanhnt <tatthanh.dev@gmail.com>
2017-11-14 23:38:07 +0700

    featured content for category page

--- resources/views/pages/web/2017/articles/category.blade.php
+++ resources/views/pages/web/2017/articles/category.blade.php
@@ -11,5 +11,135 @@
 @stop
 
 @section('content')
-    <h1>Article by Category</h1>
+    <section id="category-page">
+        <section class="wrap-content">
+            <section class="container">
+                <section id="breadcrumb">
+                    <header>
+                        <a href="/javascript"><span>J</span></a>
+                        <h2><a href="/javascript">Javascript</a></h2>
+                        <ul>
+                            <li><a href="jquery-ajax">JQuery/Ajax</a></li>
+                            <li class="active"><a href="nodejs">NodeJS</a></li>
+                            <li><a href="react-native">Reach Native</a></li>
+                            <li><a href="angular">Angular</a></li>
+                        </ul>
+                    </header>
+                    <img src="http://via.placeholder.com/730x95" alt="">
+                </section>
+
+                <section id="featured-content">
+                    <div class="row">
+                        <div class="col-lg-4 order-2" style="min-height: 300px; border: 1px solid;">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <a href="/jquery-ajax">
+                                                <img src="http://via.placeholder.com/970x250" alt="">
+                                            </a>
+                                            <figcaption>JQuery/Ajax</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ? Có gì hay ho không mà lắm thằng học thế </a>
+                                        </h5>
+                                    </article>
+                                </div>
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <img src="http://via.placeholder.com/970x250" alt="">
+                                            <figcaption>React Native</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ?</a>
+                                        </h5>
+                                    </article>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <a href="/jquery-ajax">
+                                                <img src="http://via.placeholder.com/970x250" alt="">
+                                            </a>
+                                            <figcaption>JQuery/Ajax</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ? Có gì hay ho không mà lắm thằng học thế </a>
+                                        </h5>
+                                    </article>
+                                </div>
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <img src="http://via.placeholder.com/970x250" alt="">
+                                            <figcaption>React Native</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ?</a>
+                                        </h5>
+                                    </article>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <a href="/jquery-ajax">
+                                                <img src="http://via.placeholder.com/970x250" alt="">
+                                            </a>
+                                            <figcaption>JQuery/Ajax</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ? Có gì hay ho không mà lắm thằng học thế </a>
+                                        </h5>
+                                    </article>
+                                </div>
+                                <div class="col-lg-12 col-md-6">
+                                    <article>
+                                        <figure>
+                                            <img src="http://via.placeholder.com/970x250" alt="">
+                                            <figcaption>React Native</figcaption>
+                                        </figure>
+                                        <h5>
+                                            <a href="http://localhost:8000/category/article-slug">Reactive programing là gì ?</a>
+                                        </h5>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 order-3" style="min-height: 300px; border: 1px solid;">
+                            <div class="row">
+                                <div class="col-lg-12" style="border: 1px solid;">
+                                    <img src="http://via.placeholder.com/300x550" alt="">
+                                </div>
+                                <div class="col-lg-12" style="border: 1px solid;">
+                                    <img src="http://via.placeholder.com/420x340" alt="">
+                                </div>
+                                <div class="col-lg-12" style="border: 1px solid;">
+                                    <img src="http://via.placeholder.com/420x340" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 order-4" style="min-height: 300px; border: 1px solid;">
+                            <div class="row">
+                                <div class="col-lg-12" style="min-height: 300px; border: 1px solid;">
+                                    <img src="http://via.placeholder.com/300x600" alt="">
+                                </div>
+                                <div class="col-lg-12" style="min-height: 300px; border: 1px solid;">
+                                    <img src="http://via.placeholder.com/300x600" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </section>
+
+        <section id="normal-content"></section>
+    </section>
 @stop
