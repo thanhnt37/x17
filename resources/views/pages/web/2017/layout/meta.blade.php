122bb930fa7df3f06fee322fea4abe7dd5213042
thanhnt <tatthanh.dev@gmail.com>
2017-11-20 17:28:25 +0700

    google bot noindex for development mode

--- resources/views/pages/web/2017/layout/meta.blade.php
+++ resources/views/pages/web/2017/layout/meta.blade.php
@@ -1,4 +1,10 @@
 <meta charset="utf-8">
+
+{{-- for development mode --}}
+<meta name="robots" content="noindex" />
+<meta name="googlebot" content="noindex">
+<meta name="googlebot-news" content="nosnippet">
+
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="csrf-token" content="{!! csrf_token() !!}">
