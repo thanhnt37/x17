d29c9d2c9e3c7876549f2769eae9919d2a31f960
thanhnt <tatthanh.dev@gmail.com>
2018-01-01 22:11:16 +0700

    observer update redis data

--- app/Observers/ArticleImageObserver.php
+++ app/Observers/ArticleImageObserver.php
@@ -4,26 +4,29 @@ use Illuminate\Support\Facades\Redis;
 
 class ArticleImageObserver extends BaseObserver
 {
-    protected $cachePrefix = 'article_images';
+    protected $cachePrefix = 'ArticleImageModel';
 
     public function created($model)
     {
         if( \CacheHelper::cacheRedisEnabled() ) {
-            Redis::hsetnx(\CacheHelper::generateCacheKey('hash_' . $this->cachePrefix), $model->id, $model);
+            $cacheKey = \CacheHelper::keyForModel($this->cachePrefix);
+            Redis::hsetnx($cacheKey, $model->id, $model);
         }
     }
 
     public function updated($model)
     {
         if( \CacheHelper::cacheRedisEnabled() ) {
-            Redis::hset(\CacheHelper::generateCacheKey('hash_' . $this->cachePrefix), $model->id, $model);
+            $cacheKey = \CacheHelper::keyForModel($this->cachePrefix);
+            Redis::hset($cacheKey, $model->id, $model);
         }
     }
 
     public function deleted($model)
     {
         if( \CacheHelper::cacheRedisEnabled() ) {
-            Redis::hdel(\CacheHelper::generateCacheKey('hash_' . $this->cachePrefix), $model->id);
+            $cacheKey = \CacheHelper::keyForModel($this->cachePrefix);
+            Redis::hdel($cacheKey, $model->id);
         }
     }
 }
